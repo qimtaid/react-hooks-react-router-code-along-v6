94f48b4ec24b6380d4836ee09b28a5fb7ef3f939
Dan Koskei <qimtaid@gmail.com>
2024-04-30 10:22:52 +0300

    complete lab

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+   <>
+    <header>
+        {/* sapce for Navbar */}
+    </header>
+    <main>
+        <h1>This is my About component!</h1>
+    </main>
+   </>
+  )
+}
+
+export default About
